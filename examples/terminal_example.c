98db85d50295ec08f97a3cd280c5c83110ef5d52
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2025-11-24 16:22:39 +0530

    Added infinite task for demo

--- examples/terminal_example.c
+++ examples/terminal_example.c
@@ -2,11 +2,19 @@
 #include "task.h"
 #include "memory.h"
 #include "terminal.h"
-
+#include "utils.h"
+void inf(void *args)
+{
+    while (1)
+    {
+        v_log(LOG_ERROR, "Running");
+    }
+}
 int main()
 {
     v_init();
     terminal_init();
+    register_command(inf, "inf");
     v_heap_memory_init();
     scheduler_init();
     uint32_t t2 = task_create(terminal_run, NULL, 1024, 0);
